da3c0f1aafd2254fb75481357dfe053ed7a3c7d7
dreamcmi <1912544842@qq.com>
2024-01-04 21:22:25 +0800

    fix uart code, now refactored using posix api

--- C906B/example/peripherals/uart/main.c
+++ C906B/example/peripherals/uart/main.c
@@ -1,42 +1,47 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
 #include <unistd.h>
 
-#include <wiringx.h>
-
-int main() {
-    struct wiringXSerial_t wiringXSerial = {115200, 8, 'n', 1, 'n'};
-    char buf[1024];
-    int str_len = 0;
-    int i;
-    int fd;
-
-    if(wiringXSetup("duo", NULL) == -1) {
-        wiringXGC();
+int main(void) {
+    // 打开设备文件
+    int fd = open("/dev/ttyS1", O_RDWR | O_NOCTTY);
+    if (fd == -1) {
         return -1;
     }
-
-    if ((fd = wiringXSerialOpen("/dev/ttyS4", wiringXSerial)) < 0) {
-        printf("Open serial device failed: %d\n", fd);
-        wiringXGC();
-        return -1;
+    // 获取并修改终端属性
+    struct termios options;
+    tcgetattr(fd, &options);                           // 获取当前属性
+    cfsetispeed(&options, B9600);                      // 设置输入波特率为9600
+    cfsetospeed(&options, B9600);                      // 设置输出波特率为9600
+    options.c_cflag |= (CLOCAL | CREAD);               // 设置本地模式和使能接收
+    options.c_cflag &= ~CSIZE;                         // 清除数据位掩码
+    options.c_cflag |= CS8;                            // 设置数据位为8
+    options.c_cflag &= ~PARENB;                        // 清除校验位
+    options.c_cflag &= ~CSTOPB;                        // 清除停止位
+    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);// 设置为原始模式
+    options.c_iflag &= ~(IXON | IXOFF | IXANY);        // 关闭软件流控制
+    options.c_oflag &= ~OPOST;                         // 设置为原始输出模式
+    tcsetattr(fd, TCSANOW, &options);                  // 设置新属性
+    // 写入数据
+    char *data = "Hello, world!\n";
+    int n = write(fd, data, strlen(data));
+    if (n == -1) {
+        return -2;
     }
-
-    wiringXSerialPuts(fd, "Duo Serial Test\n");
-
-    while(1)
-    {
-        str_len = wiringXSerialDataAvail(fd);
-        if (str_len > 0) {
-            i = 0;
-            while (str_len--)
-            {
-                buf[i++] = wiringXSerialGetChar(fd);
-            }
-            printf("Duo UART receive: %s\n", buf);
-        }
+    printf("写入了%d个字节\n", n);
+    // 读取数据
+    char buffer[256];
+    n = read(fd, buffer, sizeof(buffer));
+    if (n == -1) {
+        return -3;
     }
-
-    wiringXSerialClose(fd);
-
+    printf("读取了%d个字节\n", n);
+    buffer[n] = '\0';// 添加字符串结束符
+    printf("读取的数据是：%s\n", buffer);
+    // 关闭设备文件
+    close(fd);
     return 0;
 }
